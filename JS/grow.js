455043eb92bbb890a6b69dab43e1255002660959
angumah <tonsterngumah@gmail.com>
2020-04-22 17:26:26 -0400

    info test

--- JS/grow.js
+++ JS/grow.js
@@ -4,21 +4,27 @@ var particleUnlock = false;
 var particles = 0;
 var partClickValue = 1;
 var energyMax = 100;
+
 function energyClick(){
 energy += clickvalue;
 var e = document.getElementById("energy");
 e.innerHTML = energy;
+
 if (energy == energyMax) {
     document.getElementById("eButton").disabled = true;
 }
+
 else {
     document.getElementById("eButton").disabled = false;
 }
+if (energy >= 30) {
+    particleUnlock = true;
+}
+
 }
 
 function particleClick(){
-if (energy >= 100) {   
-document.getElementById("eButton").disabled = false; 
+if (energy >= 100) { 
 particleUnlock = true;  
 setInterval(myTimer, 1000);
 var e = document.getElementById("energy");
@@ -36,6 +42,7 @@ if (particleUnlock == true){
 }
 console.log(particles)
 }
+
 function myTimer(){
 energy++;
 var e = document.getElementById("energy");
