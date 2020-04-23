2199bf60876e616abda74744dde6667b9089171e
angumah <tonsterngumah@gmail.com>
2020-04-23 17:38:33 -0400

    fixed limit

--- JS/grow.js
+++ JS/grow.js
@@ -6,6 +6,7 @@ var partClickValue = 1;
 var energyMax = 100;
 var timer = false;
 var particleMax = 100;
+var timeInt = 1000;
 
 function energyClick(){
 energy += clickvalue;
@@ -15,6 +16,7 @@ e.innerHTML = energy;
 if (energy == energyMax) {
     document.getElementById("eButton").disabled = true;
     timer = false;
+    clearInterval(pTime);
 }
 
 else {
@@ -27,10 +29,9 @@ if (energy >= 30) {
 
 function particleClick(){
     if (energy >= 30) { 
-    timer = true;
-    particleUnlock = true;}
-    if (particleUnlock == true && timer == true && energy >= 30){
-        setInterval(myTimer, 1000);
+        particleUnlock = true;
+    }
+    if (particleUnlock == true && energy >= 30){
         particles += partClickValue;
         var p = document.getElementById("particle");
         p.innerHTML = particles;
@@ -38,8 +39,11 @@ function particleClick(){
         var e = document.getElementById("energy");
         e.innerHTML = energy;
     }
-    else {
-        stop(myTimer);
+    if(particleUnlock == true && energy >= 30 && energy == energyMax){
+        particles += partClickValue;
+        var p = document.getElementById("particle");
+        p.innerHTML = particles;
+        energy -= 30;
     }
     if (particle == particleMax){
         document.getElementById("pButton").disabled = true;
@@ -47,10 +51,13 @@ function particleClick(){
     else {
         document.getElementById("pButton").disabled = false;
     }
-    
+    myTimer();
+}
     function myTimer(){
+    if (energy < energyMax){
     energy++;
     var e = document.getElementById("energy");
     e.innerHTML = energy;
+    setTimeout(myTimer, 1000)
     }
-}
+    }
