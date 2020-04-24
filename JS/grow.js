ad064b35c5c609c60891351894c6eb2996227751
angumah <tonsterngumah@gmail.com>
2020-04-23 20:35:55 -0400

    fixed buttons

--- JS/grow.js
+++ JS/grow.js
@@ -86,6 +86,12 @@ function gravityClick(){
     var g = document.getElementById("gravity");
     g.innerHTML = gravity;
     gravityClickTimer();
+    energy -= 50;
+    particles -= 10;
+    var e = document.getElementById("energy");
+    e.innerHTML = energy;
+    var p = document.getElementById("particle");
+    p.innerHTML = particles;
     }
     if (gravity == gravityMax){
         document.getElementById("gravButton").disabled = true;
